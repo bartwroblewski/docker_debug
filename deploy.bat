6135d12ac288e856fcd94be599b7daed831e7b5d
bartwroblewski <barti.wroblewski@gmail.com>
2023-06-07 22:22:50 +0200

    'ok'

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,3 @@
 @REM ssh root@46.101.156.79 "cd /home && git pull https://github.com/bartwroblewski/docker_debug.git && docker-compose up"
-ssh -t root@46.101.156.79 "cd /home/docker_debug && docker-compose up"
+@REM https://medium.com/@aslav3/how-to-setup-github-actions-to-deploy-to-your-digital-ocean-server-automatically-a132d2664807
+ssh root@46.101.156.79 "cd /home/docker_debug && git pull && docker-compose build --force-rm --no-cache --compress && docker-compose up -d"
